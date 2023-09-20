690d190b86f79fcc156055187e0b550567ca2c40
Keiiiii25 <cuanan_keith@yahoo.ca>
2023-09-19 21:31:14 -0600

    Created Main Section with styling

---
+++ src/Pages/Home/MainSection.jsx
@@ -0,0 +1,17 @@
+export default function MainSection() {
+    return (
+        <section id="mainSection" className="main-section">
+            <div className="main-section-content-box">
+                <div className="main-section-content">
+                    <p className="section-title">CELEBRATE WITHOUT WORRY!</p>
+                    <h1 className="main-section-title">Kindersley Event and Party Supplies</h1>
+                </div>
+                <button className="btn btn-primary">Get In Touch</button>
+            </div>
+            <div className="main-section-img">
+                <img src="./img/main-page-img.png" alt="MainPageImage">
+                </img>
+            </div>
+        </section>
+    );
+}
